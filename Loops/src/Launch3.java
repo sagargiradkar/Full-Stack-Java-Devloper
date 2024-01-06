43bc869a394f0ea6a3476be6c3e97d96c1d0a379
sagargiradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-01-06 08:11:47 +0530

    Java 4

--- Loops/src/Launch3.java
+++ Loops/src/Launch3.java
@@ -3,7 +3,12 @@ public class Launch3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int i=5;
+		while(i<5) {
+			System.out.println("*");
+			i++;
+			
+		}
 	}
 
 }
