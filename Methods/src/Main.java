0af4870d440d21f502bf58ee854d8488dba37aaa
sagargiradkar <spgiradkar2002@gmail.com>
2024-01-14 17:54:59 +0530

    class 8

---
+++ Methods/src/Main.java
@@ -0,0 +1,34 @@
+class Calculator{
+	int a,b,c;
+	
+	void add() {
+		a=10;
+		b=20;
+		c=a+b;
+		
+		System.out.println(c);
+	}
+	void disp()
+	{
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+}
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+	Calculator calc	= new Calculator();
+	
+	calc.add();
+	
+	calc.disp();
+		
+	}
+
+}
