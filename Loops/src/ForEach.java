ecc547197f1af21ef270a9f711882f6035ad1474
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-04-14 17:24:33 +0530

    array and forEach Loop

---
+++ Loops/src/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+        int a[] = {10,20,30,40,50};
+
+        for(int elem:a){
+            System.out.println("Element :"+elem);
+        }
+    }
+}
