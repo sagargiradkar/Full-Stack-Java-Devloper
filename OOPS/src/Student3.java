6979db426be6a72babd939e05dbbeec6002b9209
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-05-28 14:52:35 +0530

    constructor overloading

---
+++ OOPS/src/Student3.java
@@ -0,0 +1,53 @@
+class Student4{
+	private String name;
+	private int rollNo;
+	private String cityName;
+	private int age;
+	
+	
+
+public Student4(String name, int rollNo, String cityName, int age) {
+		super();
+		this.name = name;
+		this.rollNo = rollNo;
+		this.cityName = cityName;
+		this.age = age;
+	}
+//	public Student4(String name, int rollNo, String cityName) {
+//		this.name = name;
+//		this.rollNo = rollNo;
+//		this.cityName = cityName;
+//	}
+//	
+//	public Student4() {
+//		name="Sagar";
+//		age = 22;
+//		cityName="Bhnadara";
+//	}
+	public String getName() {
+		return name;
+	}
+	public int getRollNo() {
+		return rollNo;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+
+}
+public class Student3 {
+
+	public static void main(String[] args) {
+	
+		Student4 std = new Student4("Sagar",3051,"Bhnadara",22);
+		
+		
+		System.out.println(std.getName());
+		System.out.println(std.getRollNo());
+		System.out.println(std.getCityName());
+		
+		//Student4 sdt2 = new Student4();
+		System.out.println();
+	}
+
+}
