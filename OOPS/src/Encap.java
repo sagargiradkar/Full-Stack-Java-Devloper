7d6e6ec6a24868f609d542484c367725e9431757
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-05-28 10:15:47 +0530

    setters and getters

---
+++ OOPS/src/Encap.java
@@ -0,0 +1,48 @@
+class Student
+{
+	//private
+	 private int age; // instqance variable/Data members/Fields
+	 private String name;//properties
+	 private String city;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+}
+
+
+public class Encap {
+
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+		Student st=new Student();
+//		st.age=30;
+//		st.name="sagar";
+//		st.city="Bengluru";
+		
+		
+		st.setAge(22);
+		st.setName("Sagar");
+		st.setCity("Bhandara");
+		System.out.println(st.getAge());
+		System.out.println(st.getName());
+		System.out.println(st.getCity());
+	
+	}
+
+}
---
+++ Practice/src/All_Alpha_Invo.java
@@ -0,0 +1,9 @@
+
+public class All_Alpha_Invo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
