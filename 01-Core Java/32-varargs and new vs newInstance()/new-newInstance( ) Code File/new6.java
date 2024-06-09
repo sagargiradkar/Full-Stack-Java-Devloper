716654bafe0ecb2433c1441c3ae663eb439969d4
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-06-09 20:16:52 +0530

    31-35

---
+++ 01-Core Java/32-varargs and new vs newInstance()/new-newInstance( ) Code File/new6.java
@@ -0,0 +1,51 @@
+//The ClassNotFoundException occurs when the classpath
+//doesn't get updated with the required JAR files.
+
+//new => requried class details known to compiler but not available at jvm then it
+//would result in "NoClassDefFoundError"
+// new loads .class file it search .class file in current working directory
+
+//newInstance() => requried class details not available at jvm then it would result
+//in "ClassNotFoundException"
+// newInstance we have to load manually it search .class file in current working directory
+
+		
+		
+
+class Student6
+{
+	
+	static {
+		System.out.println("Student .class file is loading");
+		
+	}
+	public Student6()
+	{
+		
+		System.out.println("Student constructor is called");
+	}
+}
+public class new6 {
+	static{
+	      System.out.print("Test.class file");
+	  }
+
+
+	public static void main(String[] args) throws Exception {
+		
+		// String className=args[0];
+				// Class c=Class.forName(className);
+				// Object obj=c.newInstance();
+
+				// Student5 std=(Student5)obj;
+				// System.out.println(std);
+				        // or
+				String className=args[0];
+				Object obj=Class.forName(args[0]).newInstance();
+
+				System.out.println(obj.getClass().getName());
+
+
+	}
+
+}
