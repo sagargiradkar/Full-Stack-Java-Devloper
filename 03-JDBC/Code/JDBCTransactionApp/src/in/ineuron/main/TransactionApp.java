daea2de6ce920f56e0e08956a8b94e76687ebcf1
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-06-14 18:06:21 +0530

    case Study

--- 03-JDBC/Code/JDBCTransactionApp/src/in/ineuron/main/TransactionApp.java
+++ 03-JDBC/Code/JDBCTransactionApp/src/in/ineuron/main/TransactionApp.java
@@ -2,10 +2,9 @@ package in.ineuron.main;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.sql.Statement;
 import java.util.Scanner;
 
 import in.ineuron.util.JdbcUtil;
@@ -16,71 +15,64 @@ public class TransactionApp {
 		
 		//Resource used
 		Connection connection = null;
-		PreparedStatement pstmt = null;
+		Statement stmt = null;
+		ResultSet resultSet = null;
 		Scanner sc = null;
-		
-		
-		//variable used
-		java.sql.Date sqlDob = null;
-		java.sql.Date sqlDom = null;
-		 
-		String name = null;
-		String dob = null;
-		String dom = null;
-		
+
 		try {
-			
 			connection = JdbcUtil.getJdbcConnection();
-			String sqlInsertQuery="insert into user(`id`,`name`,`dob`,`dom`) values(?,?,?,?)";
-			if(connection !=null)
-				connection.prepareStatement(sqlInsertQuery);
 			
-			if(pstmt !=null) {
-				
-				 sc = new Scanner(System.in);
-				if(sc != null) {
-					System.out.print("Enter the username :: ");
-					name = sc.next();
-					
-					System.out.println("Enter the dob (MM-dd-yyy) :: ");
-					dob = sc.next();
-					
-					System.out.println("Enter the dom(yyy-MM-dd :: ");
-					dom = sc.next();
-					
+			if(connection !=null)
+				stmt = connection.createStatement();
+			
+			if(stmt != null)
+			{
+				resultSet = stmt.executeQuery("select name,balance from accounts");
+			}
+			System.out.println("<<<<<<<<<<<< Balance before Transaction >>>>>>>>>>");
+
+			if(resultSet != null) {
+				System.out.println("NAME\tBALANCE");
+				while(resultSet.next()) {
+					System.out.println(resultSet.getString(1)+"\t"+resultSet.getInt(2));
 				}
-				if(dob != null)
-				{
-					//Conversion of string to sql Date
-					SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyy");
-					java.util.Date uDate = sdf.parse(dob);
-					
-					long value = uDate.getTime();
-					 sqlDob = new java.sql.Date(value);
+			}
+			
+			
+			//Transaction begins
+			System.out.println("\n Transactions Begins ....");
+			connection.setAutoCommit(false);
+			
+			//Prepare the operation as a single unit
+			stmt.executeUpdate("update accounts set balance = balance - 5000 where name = 'sachi'");
+			stmt.executeUpdate("update accounts set balance = balance + 5000 where name = 'dhoni'");
+			
+			System.out.println("Can you please confirm the transaction of 5000INR ...[YES/NO]");
+			sc = new Scanner(System.in);
+			String option = sc.next();
+			if(option.equalsIgnoreCase("yes")) {
+				connection.commit();
+			}else {
+				connection.rollback();
+			}
+			
+			System.out.println("<<<<<<<<<<<< Balance after Transaction >>>>>>>>>>");
+			ResultSet rs = stmt.executeQuery("select name,balance from accounts");
+			if(resultSet != null) {
+				System.out.println("NAME\tBALANCE");
+				while(rs.next()) {
+					System.out.println(rs.getString(1)+"\t"+rs.getInt(2));
 				}
-				
-				if(dom != null) {
-					sqlDom = java.sql.Date.valueOf(dom);
-				}
-				//set the input values to query  
-				pstmt.setString(1, name);
-				pstmt.setDate(2,sqlDob );
-				pstmt.setDate(3,sqlDom );
-				
-				//Excecute the query
-				int rowAffected = pstmt.executeUpdate();
-				System.out.println("No of rows inserted is ::"+rowAffected);
 			}
 		}
 		catch(SQLException |IOException e) {
 			e.printStackTrace();
-		}catch(ParseException e) {
-			e.printStackTrace();
+	
 		}catch(Exception e) {
 			e.printStackTrace();
 		}finally {
 			try {
-				JdbcUtil.cleanUp(connection, pstmt, null);
+				JdbcUtil.cleanUp(connection, stmt, null);
 			}catch(SQLException e) {
 				e.printStackTrace();
 			}
